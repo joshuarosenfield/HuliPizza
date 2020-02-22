a19fc569e6d482e872f7faaf10db7e23e2d198dd
Joshua Rosenfield <jcr15h@my.fsu.edu>
2020-02-22 10:54:11 -0500

    Pizza History Challenge

---
+++ HuliPizza/SelectedImageView.swift
@@ -0,0 +1,27 @@
+//
+//  SelectedImageView.swift
+//  HuliPizza
+//
+//  Created by Joshua Rosenfield on 2/22/20.
+//  Copyright © 2020 Steven Lipton. All rights reserved.
+//
+
+import SwiftUI
+
+struct SelectedImageView: View {
+    var image:String
+    var body: some View {
+        Image(image)
+        .resizable()
+            .scaledToFit()
+            .cornerRadius(30)
+            .shadow(color:.black, radius:10, x:5, y:5)
+        
+    }
+}
+
+struct SelectedImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedImageView(image:"1_250w")
+    }
+}
